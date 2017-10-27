da95c6025f9324568e5b6da1452c08640e039f72
Luigi Tavolaro Santini <luigisant@live.com>
2017-10-26 23:18:16 -0200

    teste git

--- CursoCSharp/Form1.cs
+++ CursoCSharp/Form1.cs
@@ -16,5 +16,10 @@ namespace CursoCSharp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
